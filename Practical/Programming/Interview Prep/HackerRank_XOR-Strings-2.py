3e65006b90d95034f6d194cdad6bc972624dc538
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:27:46 +0800

    Create HackerRank_XOR-Strings-2.py

---
+++ Practical/Programming/Interview Prep/HackerRank_XOR-Strings-2.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-strings-xor/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0';
+        else:
+            res += '1';
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
