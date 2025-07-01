52b8c89b750659a9a461d1a47ceea8f787a09dfe
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:23:18 +0800

    Create HackerRank_Flipping-Bits.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Flipping-Bits.py
@@ -0,0 +1,35 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    binary = str(bin(n)[2:]).zfill(32).replace('0', '_').replace('1', '0').replace('_', '1')
+    return int(binary, 2)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
