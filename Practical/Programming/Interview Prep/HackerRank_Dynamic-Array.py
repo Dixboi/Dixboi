c4f723918788377f4b485a50175d2f03f978a7b6
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:22:37 +0800

    Create HackerRank_Dynamic-Array.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Dynamic-Array.py
@@ -0,0 +1,59 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-dynamic-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dynamicArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def dynamicArray(n, queries):
+    lastAnswer = 0
+    arr = []
+    for i in range(n):
+        arr.append([])
+    idx = 0
+    
+    answers = []
+    
+    for _ in range(len(queries)):
+        idx = (queries[_][1] ^ lastAnswer) % n
+        if queries[_][0] == 1:
+            arr[idx].append(queries[_][2])
+        else:
+            lastAnswer = arr[idx][queries[_][2]%len(arr[idx])]
+            answers.append(lastAnswer)
+            
+    return answers
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = dynamicArray(n, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
