3e474ea78a77e0942c77ca01e525776d7a3a71fc
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:22:18 +0800

    Create HackerRank_Drawing-Book.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Drawing-Book.py
@@ -0,0 +1,46 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-drawing-book/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    if p==1 or p==n:
+        return 0
+    
+    if p == n-1 and n % 2 == 0:
+        return 1
+    
+    l = p - 1
+    r = n - p
+    
+    if l < r:
+        return p // 2
+    else:
+        return (n - p) // 2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
