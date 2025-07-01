3b3b90628d4e6d42fe7d6eea29344d6e6cfe07e5
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:25:27 +0800

    Create HackerRank_Sales-by-Match.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Sales-by-Match.py
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    socks_num = {}
+    pairs = 0
+    
+    for sock in ar:
+        socks_num[sock] = socks_num.get(sock, 0) + 1
+    
+    for key in socks_num.values():
+        pairs += key // 2
+    
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
