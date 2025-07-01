3169d2088587c78783ababef7ee3a0c00ae32cb6
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:21:50 +0800

    Create HackerRank_Diagonal-Difference.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Diagonal-Difference.py
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    length = len(arr)
+    pr_diag = 0
+    sc_diag = 0
+    
+    for _ in range(length):
+        pr_diag += arr[_][_]
+        sc_diag += arr[_][length-1-_]
+    
+    return abs(pr_diag-sc_diag)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
