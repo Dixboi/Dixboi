ae5a6765c8f62a5dd9ce04c0257e84f2fe2612e8
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:27:02 +0800

    Create HackerRank_Subaaray-Division-1.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Subaaray-Division-1.py
@@ -0,0 +1,48 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-the-birthday-bar/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    segments = 0
+    
+    for _ in range(len(s)-m+1):
+        if sum(s[_:_+m]) == d:
+            segments += 1
+        
+    
+    return segments
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
