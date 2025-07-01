dfa75d57688774db8e642c9b2389b4baf60170da
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:21:30 +0800

    Create HackerRank_Counting-Sort-1.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Counting-Sort-1.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    counts = [0] * 100
+    for _ in arr:
+        counts[_] += 1
+    
+    return counts
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
