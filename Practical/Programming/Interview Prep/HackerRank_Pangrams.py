06d5532974295ee9ecd8040f8c4fb833bd97b2e0
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:24:44 +0800

    Create HackerRank_Pangrams.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Pangrams.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-pangrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    s = s.lower().replace(" ", "")
+    
+    letters = [0] * 26
+    
+    for _ in s:
+        letters[ord(_) - ord("a")] += 1
+    
+    return "pangram" if letters.count(0) == 0 else "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
