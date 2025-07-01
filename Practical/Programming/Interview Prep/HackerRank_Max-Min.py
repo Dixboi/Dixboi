d2729f1d813265b1d5ae6a9a3a3bbc6ffdd0e324
Dex <59426458+Dixboi@users.noreply.github.com>
2025-07-01 16:24:12 +0800

    Create HackerRank_Max-Min.py

---
+++ Practical/Programming/Interview Prep/HackerRank_Max-Min.py
@@ -0,0 +1,49 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-angry-children/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
+#
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    arr.sort()
+    
+    uf_min = 10000000000
+    
+    for _ in range(len(arr)-k+1):
+        uf_temp = arr[_+k-1] - arr[_]
+        if uf_temp < uf_min:
+            uf_min = uf_temp
+        
+    return uf_min
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
